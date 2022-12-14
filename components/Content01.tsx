5f60845e2f2260ae116e6356fcf2db0014ffac52
whroyal <af.jaguarking@gmail.com>
2022-12-14 17:11:10 -0500

    intropage

--- components/Content01.tsx
+++ components/Content01.tsx
@@ -7,20 +7,30 @@ const Content01 = (props:any) => {
         <div className="text-[25px] leading-[30px] md:text-[30px] md:leading-[45px] xl:text-[50px] xl:leading-[60px]
            font-medium text-[#f5dc8b] text-shadow1 text-left px-4">
             <div id='container'>
-              Welcome to my website
+              Hey
             </div>
           <div className="mt-80 md:mt-20 w-fit mx-auto">
             <span className="text-[40px] md:text-[50px] xl:text-[70px] font-bold text-[#f5dc8b]">I’m</span>
             <span className="text-[50px] md:text-[70px] xl:text-[90px] font-bold text-[#FFFFFF]"> Dmytro Kornukh</span>
           </div>
-          <div className="mt-12 text-[#f5dc8b]">
-          Borys Grinchenko Kyiv University <br className="md:hidden"/>
-          </div>
-          <div className="pl-4 text-[#FFFEFE]">2009 ~ 2013</div>
-          <div className="text-[#FFFEFE] mt-5">
-            Studied mathematics and computer science.<br/>
-            Have been awarded Bachelor’s degree
-          </div>
+          {/* <div className="mt-12 text-[#f5dc8b]">
+           <br className="md:hidden"/>
+          </div> */}
+
+            <div className="flowtext">
+              <div className="flowtext__container">
+                <p className="flowtext__container__text">
+                  Hello
+                </p>
+                
+                <ul className="flowtext__container__list">
+                  <li className="flowtext__container__list__item">world !</li>
+                  <li className="flowtext__container__list__item">bob !</li>
+                  <li className="flowtext__container__list__item">users !</li>
+                  <li className="flowtext__container__list__item">everybody !</li>
+                </ul>
+              </div>
+            </div>
         </div>
       </div>
     </div>
